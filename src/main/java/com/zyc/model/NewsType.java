06337ce65b9c1a93b25ff36cb3bd938716235925
zyc <739781049@qq.com>
2017-09-25 16:46:26 +0800

    增加新闻类型的枚举

---
+++ src/main/java/com/zyc/model/NewsType.java
@@ -0,0 +1,26 @@
+package com.zyc.model;
+
+/**
+ * Created by YuChen Zhang on 17/09/25.
+ */
+public enum NewsType {
+    ALLNEWS ("all"),
+    VIDEO("video"),
+    IMG("img"),
+    GUONEI("guonei"),
+    GUOWAI("guowai"),
+    SHEHUI("shehui"),
+    TIYU("tiyu"),
+    CAIJING("caijing"),
+    YULE("yule"),
+    KEJI("keji");
+
+    private String type;
+    NewsType(String input) {
+        this.type=input;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
