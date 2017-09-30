cf8d395751abe2a4f1dae2469abd9845ffbc83ee
zyc <739781049@qq.com>
2017-09-30 12:03:01 +0800

    权限管理服务层

---
+++ src/main/java/com/zyc/service/PowerService.java
@@ -0,0 +1,14 @@
+package com.zyc.service;
+
+import com.zyc.model.Power;
+import com.zyc.model.Role;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by YuChen Zhang on 17/09/29.
+ */
+public interface PowerService {
+    List<Power> getPowerByRoles(List<Role> roles);
+}
---
+++ src/main/java/com/zyc/service/PowerServiceImplements.java
@@ -0,0 +1,46 @@
+package com.zyc.service;
+
+import com.zyc.mapper.PowerMapper;
+import com.zyc.mapper.RoletopowerMapper;
+import com.zyc.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by YuChen Zhang on 17/09/29.
+ */
+@Service("powerServiceImplements")
+public class PowerServiceImplements implements  PowerService{
+
+    @Autowired
+    RoletopowerMapper roletopowerMapper;
+
+    @Autowired
+    PowerMapper powerMapper;
+
+    @Override
+    public List<Power> getPowerByRoles(List<Role> roles) {
+        List<Integer> roleid = new ArrayList<Integer>();
+        for(Role temp : roles){
+            roleid.add(temp.getRoleid());
+        }
+        //获取当前用户角色的所有权限id
+        RoletopowerExample roletopowerExample = new RoletopowerExample();
+        roletopowerExample.getOredCriteria().add(roletopowerExample.createCriteria().andRoleidIn(roleid));
+        List<Roletopower> roletopowers = roletopowerMapper.selectByExample(roletopowerExample);
+        List<Integer> poserid = new ArrayList<Integer>();
+        for(Roletopower temp : roletopowers){
+            poserid.add(temp.getPowerid());
+        }
+        //根据id获取所有的权限
+        PowerExample powerExample = new PowerExample();
+        powerExample.getOredCriteria().add(powerExample.createCriteria().andPoweridIn(poserid));
+        List<Power> powers = powerMapper.selectByExample(powerExample);
+
+        return powers;
+    }
+}
