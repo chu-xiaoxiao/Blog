44e2a0da0ff0c44e2d53f055c105026fad93955b
zyc <739781049@qq.com>
2017-09-30 12:03:13 +0800

    角色管理服务层

---
+++ src/main/java/com/zyc/service/RoleService.java
@@ -0,0 +1,26 @@
+package com.zyc.service;
+
+import com.zyc.model.Role;
+import com.zyc.model.User;
+import com.zyc.model.Usertorole;
+import com.zyc.util.MyException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by YuChen Zhang on 17/09/29.
+ */
+
+public interface RoleService {
+    List<Role> getRolesByUserName(String username) throws MyException;
+    void InsertUserToRole(Usertorole usertorole);
+
+    /**
+     * 用户授权
+     * @param user
+     * @param role
+     * @return
+     */
+    int authorization(User user,Role role);
+}
