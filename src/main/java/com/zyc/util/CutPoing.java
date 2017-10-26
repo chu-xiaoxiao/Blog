5b9c2b26567760a22507555925939a1f8f96f38f
zyc <739781049@qq.com>
2017-10-26 14:54:40 +0800

    自定义aop

--- src/main/java/com/zyc/util/CutPoing.java
+++ src/main/java/com/zyc/util/CutPoing.java
@@ -1,11 +1,16 @@
 package com.zyc.util;
 
+import com.zyc.mapper.LogrecordMapper;
+import com.zyc.model.Logrecord;
 import com.zyc.model.User;
+import com.zyc.service.LogRecordService;
 import org.aopalliance.intercept.Joinpoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -22,7 +27,9 @@ import java.lang.reflect.Method;
 @Aspect
 @Component("cutPoing")
 public class CutPoing {
-
+    @Autowired
+    @Qualifier("logRecordServiceImplements")
+    LogRecordService logRecordService;
 
     @Pointcut("@annotation(com.zyc.util.LogAop)")
     public void controllerAspect(){
@@ -30,7 +37,7 @@ public class CutPoing {
     }
 
     @Around( "execution(* com.zyc.service.*.* (..))")
-    public Object doBefore(ProceedingJoinPoint joinpoint){
+    public Object doAround(ProceedingJoinPoint joinpoint){
         System.out.println("日志aop开始========");
         Object result = null;
         try {
@@ -44,19 +51,24 @@ public class CutPoing {
 
     @Around("execution(* com.zyc.service.*.* (..))&&@annotation(logAop)")
     public Object  doAroundWithParam(ProceedingJoinPoint joinPoint,LogAop logAop){
-
         RequestAttributes ra = RequestContextHolder.getRequestAttributes();
         ServletRequestAttributes sra = (ServletRequestAttributes) ra;
         HttpServletRequest request = sra.getRequest();
-
-        System.out.println("带参数的日志aop开始========");
+        Logrecord logrecord = new Logrecord();
         Object result = null;
         try {
             User user = (User) request.getSession().getAttribute("user");
+            logrecord.setAction(logAop.CRDU().getCRDU());
+            logrecord.setIp(IPUtils.getIpaddr(request));
+            logrecord.setLogrecord(logAop.logRecord());
+            logrecord.setTablename(logAop.tableName());
+            if(user!=null) {
+                logrecord.setUserid(user.getId());
+                logrecord.setUsername(user.getUsername());
+            }
+            logRecordService.insert(logrecord);
             result = joinPoint.proceed();
             System.out.println(request.getRequestURL());
-            System.out.println(logAop.ip()+user.getUsermail());
-            System.out.println("带参数的日志aop结束========");
         } catch (Throwable throwable) {
             throwable.printStackTrace();
         }
